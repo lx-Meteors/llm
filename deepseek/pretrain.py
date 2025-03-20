b562c6d646a197ba715b3b3c8641a2db7139863e
Meteor <1762204162@qq.com>
2025-03-20 22:05:52 +0800

    添加lora | 添加参数解析

--- deepseek/pretrain.py
+++ deepseek/pretrain.py
@@ -3,14 +3,24 @@ import torch
 from datasets import load_dataset
 from transformers import AutoTokenizer, TrainingArguments, AutoModelForCausalLM, Trainer, \
     DataCollatorForLanguageModeling
+from peft import LoraConfig, get_peft_model
 
-tokenizer = AutoTokenizer.from_pretrained("/openbayes/input/input0/DeepSeek-R1-Distill-Qwen-1.5B")
+# 定义路径
+class PreTrainArguments:
+    model_name = "/openbayes/input/input0/DeepSeek-R1-Distill-Qwen-1.5B"
+    train_data_path = "/openbayes/home/opentest/datasets/Sky_train.jsonl"
+    test_data_path = "/openbayes/home/opentest/datasets/Sky_test.jsonl"
+
+args = PreTrainArguments()
+
+# 定义tokenizer
+tokenizer = AutoTokenizer.from_pretrained(args.model_name)
 
 # 加载数据集
 # streaming是有惰性的，不会立即执行，只有在需要的时候才会执行
 data_files = {
-    "train": "/openbayes/home/opentest/datasets/Sky_train.jsonl",
-    "test": "/openbayes/home/opentest/datasets/Sky_test.jsonl"
+    "train": args.train_data_path,
+    "test": args.test_data_path
 }
 dataset = load_dataset("json",data_files=data_files)
 print(dataset)
@@ -25,6 +35,7 @@ test_dataset = dataset["test"]
 
 train_dataset = train_dataset.map(tokenize_to_id, batched=True)
 test_dataset = test_dataset.map(tokenize_to_id, batched=True)
+train_dataset = train_dataset.select(range(10000))
 print(train_dataset)
 data_collator = DataCollatorForLanguageModeling(tokenizer, mlm=False)
 
@@ -40,15 +51,24 @@ training_args = TrainingArguments(
     evaluation_strategy='steps',       # 评估策略
     save_strategy='epoch',             # 保存策略
     learning_rate=1e-4,                # 学习率
-    eval_steps=100                     # 多少步评估一次
+    eval_steps=1000000                     # 多少步评估一次
 )
+
 
 # 定义模型
 deepseek = AutoModelForCausalLM.from_pretrained("/openbayes/input/input0/DeepSeek-R1-Distill-Qwen-1.5B")
-for name, param in deepseek.named_parameters():
-    if name == "lm_head.weight":
-        continue
-    param.requires_grad = False
+# 定义LoRA的配置
+lora_config = LoraConfig(
+    r=16,                       # LoRA秩 (越大表示保留更多信息)
+    lora_alpha=32,             # 缩放因子
+    target_modules=["q_proj", "v_proj"],  # 指定哪些层使用LoRA
+    lora_dropout=0.05,         # Dropout以防止过拟合
+    bias="none",               # 不更新偏置
+    task_type="CAUSAL_LM"      # 指定任务类型
+)
+
+deepseek = get_peft_model(deepseek, lora_config)
+deepseek.print_trainable_parameters()
 
 # 开始训练!
 trainer = Trainer(
